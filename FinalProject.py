180ea442342da24dafc77e2161fc65b0d1cf6628
Maria Paula <ps2525@ic.ac.uk>
2025-12-16 20:52:00 +0000

    Update limits

--- FinalProject.py
+++ FinalProject.py
@@ -95,7 +95,7 @@ def create_model():
 
     m.C_Amine = pyo.Constraint(expr=m.n['NH2 (primary)'] + m.n['NH (sec)'] >= 1)
     m.C_Struc = pyo.Constraint(expr=sum(m.n[g] for g in m.G) >= 3) # Minimum 3 groups
-    m.C_TotalSize = pyo.Constraint(expr=sum(m.n[g] for g in m.G) <= 8)
+    m.C_TotalSize = pyo.Constraint(expr=sum(m.n[g] for g in m.G) <= 5)
 
     # Valence/Structural Feasibility Rule
     # Sum(Ni * (2 - Vi)) = 2
